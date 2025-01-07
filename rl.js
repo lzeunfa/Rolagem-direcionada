8e309935483bd88f6ce7ffa33179fd7c617a05cf
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-01-07 12:11:41 -0300

    Update rl.js

--- rl.js
+++ rl.js
@@ -1,11 +1,11 @@
-document.querySelectorAll(`a[href^="#"]`).forEach(anchor => {
-    anchor.addEventListener(`click`, function (e) {
-        e.preventDefault();
+document.querySelectorAll(`a[href^="#"]`)/*serve para encontrar todos os links que começam com #, que são os de navegação*/.forEach(anchor/*âncora*/ => {
+    anchor.addEventListener(`click`, function (e)/*diz para o navegador fazer algo quando clicar em um link*/ {
+        e.preventDefault();//impede de fazer a ação padrão
         const targetId = this.getAttribute(`href`);
         const targetElement = document.querySelector(targetId);
         window.scrollTo({
             top: targetElement.offsetTop,
             behavior: "smooth"
-        })
+        });
     });
 });
