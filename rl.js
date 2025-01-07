c5750d97f24816432e991ed460451ebc0f14d6ef
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-01-07 11:51:36 -0300

    feito

---
+++ rl.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(`a[href^="#"]`).forEach(anchor => {
+    anchor.addEventListener(`click`, function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute(`href`);
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth"
+        })
+    });
+});
